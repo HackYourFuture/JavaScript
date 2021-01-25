2612abfaf33429d26156157594a1c764dc6c4eca
robvk <r.vankruijsdijk@gmail.com>
2021-01-25 16:41:40 +0100

    removed the practice exercises that are homework

--- Week4/practice-exercises/solutions/3-lemon-allergy.js
+++
@@ -1,9 +0,0 @@
-const fruitBasket = ['Apple', 'Lemon', 'Grapefruit', 'Lemon', 'Banana', 'Watermelon', 'Lemon'];
-
-const removeLemons = (fruitList) => {
-    const fruitListWithoutLemons = fruitList.filter(fruit => fruit !== 'Lemon');
-
-    return `My mom bought me a fruit basket, container ${fruitListWithoutLemons.join(', ')}!`;
-}
-
-console.log(removeLemons(fruitBasket));
