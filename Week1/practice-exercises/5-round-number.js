ce429e9e635a1c817e70ee0aa677b9f5baddbb90
Sina Boby <sinaboby91@yahoo.com>
2021-12-05 23:57:12 +0100

    fifth completed

--- Week1/practice-exercises/5-round-number.js
+++ Week1/practice-exercises/5-round-number.js
@@ -3,15 +3,16 @@
  */
 
  // 1. Declare a variable z and assign the number 7.25 to it.
-
+const z = 7.25 ;
 
  // 2. Write a console.log statement in which you log the value of z.
-
+console.log(z);
 
  // 3. Declare another variable a that has the value of z but rounded to the nearest integer.
-
+const a = Math.round(z);
 
  // 4. Write a console.log statement in which you log the value of a.
-
+console.log(a);
 
  // 5. So now we have z and a find a way to compare the two values and log true if a is greater than z or false if a is smaller than z.
+console.log(a > z);
