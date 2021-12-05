64ca58f2d7058303ffc2dd8c6cdfad6a4e445ab7
Sina Boby <sinaboby91@yahoo.com>
2021-12-06 00:05:58 +0100

    Seventh Completed

--- Week1/practice-exercises/7-log-string-length.js
+++ Week1/practice-exercises/7-log-string-length.js
@@ -3,7 +3,8 @@
  */
 
 // 1. Declare a variable called mySentence and initialize it with the following string: "Programming is so interesting!".
-
+let mySentence = "Programming is so interesting!";
 
 // 2. Figure out (using Google) how to get the length of mySentence. Then write a console.log statement to log the length of mySentence.
 
+console.log(mySentence.length);
