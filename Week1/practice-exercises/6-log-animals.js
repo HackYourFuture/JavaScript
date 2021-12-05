af7c4b4f8210696ec7a74d492717245b1fc35fbd
Sina Boby <sinaboby91@yahoo.com>
2021-12-06 00:03:42 +0100

    Sixth completed

--- Week1/practice-exercises/6-log-animals.js
+++ Week1/practice-exercises/6-log-animals.js
@@ -3,23 +3,23 @@
  */
 
 // 1. Declare variable and assign to it an empty array. Make sure that the name of the variable indicates it contains more than 1 item. For example items instead of item.
-
+let items = [];
 
 // 2. Write a console.log statement that explains in words what you think the value of the array is.
-
+console.log("it has undefined value yet");
 
 // 3. Write a console.log statement that logs the array.
 
-
+console.log(items);
 // 4. Create a new variable with an array that has 3 of your favorite animals, each in a different string. Make sure the name of the variables says something about what the variable contains.
-
+let animal =['lion','tiger','bear'];
 
 // 5. Write a console.log statement that logs the second array.
-
+console.log(animal);
 
 // 6. Add a statement that adds another string ("Piglet)" to the array of animals.
-
+animal.unshift('elephant');
 
 // 7. Write a console.log statement that logs the second array!
 
-
+console.log(animal);
