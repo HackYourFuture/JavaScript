bec522be454e7286a0747643539e0058325b8350
Sina Boby <sinaboby91@yahoo.com>
2021-12-05 23:52:07 +0100

    Forth one Completed

--- Week1/practice-exercises/4-log-string.js
+++ Week1/practice-exercises/4-log-string.js
@@ -4,20 +4,20 @@
  */
 
 // 1. Declare a variable myString and assign a string to it. Use your full name, including spaces, as the content for the string.
-
+let myString = 'Sina Gholipour';
 
 // 2. Write a console.log statement in which you explain in words what you think the value of the string is.
-
+console.log('I think the content of it is string data type & is my Name.');
 
 // 3. Now console.log the variable myString.
-
+console.log(myString);
 
 // 4. Now reassign to the variable myString a new string.
 
-
+myString = 'Sina Boby'; 
 // 5. Just like what you did before write a console.log statement that explains in words what you think will be logged to the console.
 
-
+console.log('I think the value is the second string which I just assigned to it');
 // 6. Now console.log myString again.
 
-
+console.log(myString);
