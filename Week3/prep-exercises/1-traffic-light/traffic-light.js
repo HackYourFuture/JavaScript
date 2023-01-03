6ed36ee9047234014c21997a329189fb0fd1ee97
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-01-03 12:22:37 +0100

    done

--- Week3/prep-exercises/1-traffic-light/traffic-light.js
+++ Week3/prep-exercises/1-traffic-light/traffic-light.js
@@ -9,6 +9,7 @@ function getCurrentState(trafficLight) {
   // TODO
   // Should return the current state (i.e. colour) of the `trafficLight`
   // object passed as a parameter.
+  return trafficLight.possibleStates[trafficLight.stateIndex];
 }
 
 function getNextStateIndex(trafficLight) {
@@ -17,6 +18,17 @@ function getNextStateIndex(trafficLight) {
   // - if the color is green, it will turn to orange
   // - if the color is orange, it will turn to red
   // - if the color is red, it will turn to green
+
+  if (trafficLight.stateIndex === 0) {
+    trafficLight.stateIndex = 1;
+    return trafficLight.stateIndex;
+  } else if (trafficLight.stateIndex === 1) {
+    trafficLight.stateIndex = 2;
+    return trafficLight.stateIndex;
+  } else if (trafficLight.stateIndex === 2) {
+    trafficLight.stateIndex = 0;
+    return trafficLight.stateIndex;
+  }
 }
 
 // This function loops for the number of seconds specified by the `secs`
