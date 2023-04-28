c197ad6ed28a02c55db6d073bd4316dba4edd86a
robvk <r.vankruijsdijk@gmail.com>
2023-04-28 13:29:50 +0200

    Add week4

---
+++ Week4/prep-exercises/1-wallet/ex4-object-shared-methods.js
@@ -0,0 +1,65 @@
+import eurosFormatter from './euroFormatter.js';
+
+function deposit(amount) {
+  this._cash += amount;
+}
+
+function withdraw(amount) {
+  if (this._cash - amount < 0) {
+    console.log(`Insufficient funds!`);
+    return 0;
+  }
+
+  this._cash -= amount;
+  return amount;
+}
+
+function transferInto(wallet, amount) {
+  console.log(
+    `Transferring ${eurosFormatter.format(amount)} from ${
+      this._name
+    } to ${wallet.getName()}`
+  );
+  const withdrawnAmount = this.withdraw(amount);
+  wallet.deposit(withdrawnAmount);
+}
+
+function reportBalance() {
+  console.log(
+    `Name: ${this._name}, balance: ${eurosFormatter.format(this._cash)}`
+  );
+}
+
+function getName() {
+  return this._name;
+}
+
+function createWallet(name, cash = 0) {
+  return {
+    _name: name,
+    _cash: cash,
+    deposit,
+    withdraw,
+    transferInto,
+    reportBalance,
+    getName,
+  };
+}
+
+function main() {
+  const walletJack = createWallet('Jack', 100);
+  const walletJoe = createWallet('Joe', 10);
+  const walletJane = createWallet('Jane', 20);
+
+  walletJack.transferInto(walletJoe, 50);
+  walletJane.transferInto(walletJoe, 25);
+
+  walletJane.deposit(20);
+  walletJane.transferInto(walletJoe, 25);
+
+  walletJack.reportBalance();
+  walletJoe.reportBalance();
+  walletJane.reportBalance();
+}
+
+main();
