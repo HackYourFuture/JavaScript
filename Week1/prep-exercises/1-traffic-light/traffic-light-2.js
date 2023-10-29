64c7f039eb45f74528fdcfa2d8dda7b4d192298b
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2023-10-29 23:51:14 +0100

    Update traffic-light-2.js
    edited code to required output

--- Week1/prep-exercises/1-traffic-light/traffic-light-2.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-2.js
@@ -14,6 +14,14 @@ while (cycle < 2) {
   const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
   console.log("The traffic light is on", currentState);
 
+if(currentState == "green"){
+  trafficLight.stateIndex ++;
+}else if(currentState == "orange") {
+  trafficLight.stateIndex ++;
+}else{
+  trafficLight.stateIndex -= 2;
+  cycle++;
+}
   // TODO
   // if the color is green, turn it orange
   // if the color is orange, turn it red
