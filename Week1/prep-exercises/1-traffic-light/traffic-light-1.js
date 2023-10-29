ec47026d0226c26834b274e4d0bebae0efae9dca
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2023-10-29 23:42:52 +0100

    Update traffic-light-1.js
    changed the code to required output

--- Week1/prep-exercises/1-traffic-light/traffic-light-1.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-1.js
@@ -3,7 +3,7 @@
  * The `state` property says what the traffic light's state (i.e. colour) is at
  * that moment.
  */
-const trafficLight = {
+let trafficLight = {
   state: "green",
 };
 
@@ -11,7 +11,14 @@ let rotations = 0;
 while (rotations < 2) {
   const currentState = trafficLight.state;
   console.log("The traffic light is on", currentState);
-
+if(currentState == "green"){
+  trafficLight.state = "orange";
+} else if(currentState == "orange"){
+  trafficLight.state = "red";
+}else{
+  rotations++;
+  trafficLight.state = "green";
+}
   // TODO
   // if the color is green, turn it orange
   // if the color is orange, turn it red
