1bb3bf0370983109dfa17fa5b1f2b0eef3add11c
hhana1-a <hhana1@live.com>
2024-02-10 21:57:42 +0100

    excercize

--- Week3/prep-exercises/1-hyf-program/2-class-list.js
+++ Week3/prep-exercises/1-hyf-program/2-class-list.js
@@ -1,21 +1,29 @@
 import { modules, students, mentors, classes } from "./hyf.js";
 
-/**
- * We would like to have a list of everyone that is currently participating in a class.
- * This means the students, but also the mentors that are currently teaching the class.
- * The students should be self explanatory, but to find the mentors you will need to follow these steps:
- * - Check what the `currentModule` of the class is
- * - Find the mentor(s) that are `nowTeaching` that module
- *
- * Should return the list of names and their roles. So something like:
- *
- *  [{ name: 'John', role: 'student' }, { name: 'Mary', role: 'mentor' }]
- */
-const getPeopleOfClass = (className) => {
-  // TODO complete this function
-};
-// You can uncomment out this line to try your function
-// console.log(getPeopleOfClass('class34'));
+
+
+  const getPeopleOfClass = (className) => {
+    const classInfo = classes.find((cls) => cls.name === className);
+     
+    const classStudents = students
+      .filter((student) => student.class === className)
+      .map((student) => ({ name: student.name, role: 'student' }));
+  
+    const classMentors = mentors
+      .filter((mentor) => {
+        if (!mentor.nowTeaching) return false;
+        const mentorTeachingModule = mentor.nowTeaching;
+        return mentor.canTeach.includes(classInfo.currentModule);
+      })
+      .map((mentor) => ({ name: mentor.name, role: 'mentor' }));
+  
+    return [...classStudents, ...classMentors];
+  };
+ console.log(getPeopleOfClass('class34'));
+
+ console.log(classes);
+console.log(students);
+console.log(mentors);
 
 /**
  * We would like to have a complete overview of the current active classes.
@@ -30,7 +38,25 @@ const getPeopleOfClass = (className) => {
  *  }
  */
 const getActiveClasses = () => {
-  // TODO complete this function
-};
-// You can uncomment out this line to try your function
-// console.log(getActiveClasses());
+
+  const activeClasses = classes.filter((cls) => cls.active);
+      const activeClassesInfo = {};
+    
+      activeClasses.forEach((cls) => {
+        const peopleOfClass = getPeopleOfClass(cls.name);
+        activeClassesInfo[cls.name] = peopleOfClass;
+      });
+    
+      return activeClassesInfo;
+    };
+    
+    const peopleOfClass34 = getPeopleOfClass('class34');
+    const studentNames = peopleOfClass34.map((person) => person.name).join(', ');
+    const studentID = `${studentNames} role: student`;
+    console.log(studentID);
+    
+    const activeClasses = getActiveClasses();
+    console.log(activeClasses);
+
+
+console.log(getActiveClasses());
