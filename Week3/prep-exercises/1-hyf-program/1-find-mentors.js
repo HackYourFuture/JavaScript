9f3a5adc3cc2ffcab1f1a937f64ba0f33bff7be1
aziz <aazizsigar@gmail.com>
2024-05-04 08:27:13 +0200

    prep1-2done

--- Week3/prep-exercises/1-hyf-program/1-find-mentors.js
+++ Week3/prep-exercises/1-hyf-program/1-find-mentors.js
@@ -1,26 +1,27 @@
 import { modules, students, mentors, classes } from "./hyf.js";
-
-/**
- * Tjebbe would like help to get a list of possible mentors for a module.
- * Fill in this function that finds all the mentors that can teach the given module.
- *
- * It should return an array of names. So something like:
- *  ['John', 'Mary']
- */
 const possibleMentorsForModule = (moduleName) => {
-  // TODO complete this function
+  return mentors.reduce((mentorsForModule, mentor) => {
+    if (mentor && mentor.modules && mentor.modules.includes(moduleName)) {
+      mentorsForModule.push(mentor.name);
+    }
+    return mentorsForModule;
+  }, []);
 };
-// You can uncomment out this line to try your function
-// console.log(possibleMentorsForModule('using-apis'));
 
-/**
- * Tjebbe wants to make it even easier for himself.
- * Fill in this function that chooses a random mentor to teach the given module.
- *
- * It should return a single name.
- */
+
 const findMentorForModule = (moduleName) => {
-  // TODO complete this function
+  const mentorsForModule = possibleMentorsForModule(moduleName);
+  if (mentorsForModule.length === 0) {
+    return "No mentor available for this module";
+  }
+  const randomIndex = Math.floor(Math.random() * mentorsForModule.length);
+  return mentorsForModule[randomIndex];
 };
-// You can uncomment out this line to try your function
-// console.log(findMentorForModule('javascript'));
+
+
+
+console.log(possibleMentorsForModule('using-apis'));
+
+console.log(findMentorForModule('javascript'));
+
+  
